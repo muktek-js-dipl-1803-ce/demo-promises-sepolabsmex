5016b3b77b5b1a6a093e2fa44a1dddb1cda9e989
Yair <rogues@es6.today>
2017-10-17 14:40:31 -0500

    add solution

--- js/app.js
+++ js/app.js
@@ -1,8 +1,55 @@
+// OLD WAY
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET', 'http://sepomex.icalialabs.com/api/v1/states');
+// xhr.send()
+//
+// xhr.onreadystatechange = function() {
+//   if (xhr.status === 200 && xhr.responseText) {
+//     console.log("Nice");
+//     console.log(JSON.parse(xhr.responseText).states);
+//   }
+//
+//   if (xhr.status >= 400) {
+//     console.log("error");
+//   }
+// }
 
 
-document.querySelector('#mex-city-count tbody').innerHTML = `
-  <tr>
-    <td>Colima</td>
-    <td>10</td>
-  </tr>
-`
+const request = superagent;
+let statesTable = document.querySelector('#mex-city-count tbody')
+
+// document.querySelector('#getData').addEventListener('click', function() {
+  request
+    .get('https://sepomex.icalialabs.com/api/v1/cities')
+    .then(function saveStatesInAVariable(data) {
+      const states = data.body.states
+
+      console.log(states);
+
+      return states
+    })
+    .then(function printInWebpage(statesList) {
+      statesList.forEach(function(obj) {
+        statesTable.innerHTML += `
+        <tr>
+          <td>${obj.name}</td>
+          <td>${obj.cities_count}</td>
+        </tr>`
+      })
+    })
+    .catch(function() {
+      var elem = document.createElement('p')
+      elem.innerHTML = 'Error al momento de obtener los datos'
+
+      document.querySelector('.errors').appendChild(elem)
+    })
+// })
+
+
+    // for (var i = 0; i < states.length; i++) {
+    //   var obj = states[i]
+    //
+    //   console.log( obj );
+    // }
+
+  // })
